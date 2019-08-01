bdd456b37927d8cb573d6c8b99923bd1c0a48bff
Virginia Dooley <vcdooley@gmail.com>
2019-07-31 19:55:07 -0700

    move app controller into v1

---
+++ app/controllers/api/v1/boards_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::BoardsController < ApplicationController
+
+  before_action :set_board, only: [:show]
+
+  # GET /boards
+  def index
+    @boards = Board.all
+
+    render json: @boards
+  end
+
+  # GET /boards/1
+  def show
+    render json: @board
+  end
+
+  # POST /boards
+  def create
+    binding.pry
+    @board = Board.new(board_params)
+
+    if @board.save
+      render json: @board
+    else
+      render json: @board.errors
+    end
+  end
+
+  # PATCH/PUT /boards/1
+  def update
+    if @board.update(board_params)
+      render json: @board
+    else
+      render json: @board.errors
+    end
+  end
+
+  # DELETE /boards/1
+  # def destroy
+  #   @board.destroy
+  # end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_board
+      @board = Board.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def board_params
+      params.require(:board).permit(:title, image_attributes: [:images])
+    end
+end
